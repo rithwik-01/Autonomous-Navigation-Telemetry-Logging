536b22f9c8faddcbea26e7af526e62e9090dd0bc
rithwik-01 <rithwikreddy.eedula@sjsu.edu>
2025-12-17 13:45:00 +0000

    Add waypoint navigation and telemetry scripts

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/reedula/Desktop/catkin_ws_waypoint/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/reedula/Desktop/catkin_ws_waypoint/devel;/opt/ros/kinetic'.split(';') if '/home/reedula/Desktop/catkin_ws_waypoint/devel;/opt/ros/kinetic' != '' else []
