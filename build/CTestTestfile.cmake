19da5b95a8c8bd0903c1aaccd5f7e7ef18f8b58b
rithwik-01 <rithwikreddy.eedula@sjsu.edu>
2025-12-22 11:20:00 +0000

    Add build artifacts and workspace files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/reedula/Desktop/catkin_ws_waypoint/src
+# Build directory: /home/reedula/Desktop/catkin_ws_waypoint/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(turtlebot_simulator/turtlebot_gazebo)
+subdirs(turtlebot_simulator/turtlebot_simulator)
+subdirs(turtlebot_simulator/turtlebot_stage)
+subdirs(turtlebot_simulator/turtlebot_stdr)
+subdirs(turtlebot_waypoint)
